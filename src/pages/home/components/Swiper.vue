80a475da629ea1cd67d172700d8032078d0a7a1a
YoungHorn1999 <1026344206@qq.com>
2020-07-26 00:31:58 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrapper">
+        <swiper ref="mySwiper" :options="swiperOptions">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl"/>
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data() {
+        return {
+            swiperOptions: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [
+                {
+                    id: "0001",
+                    imgUrl: require("../../../assets/img/1.jpg")
+                },
+                {
+                    id: "0002",
+                    imgUrl: require("../../../assets/img/2.jpg")
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff
+    .wrapper
+        width: 100%
+        height: 31.25vw
+        .swiper-img 
+            width: 100%
+</style>
