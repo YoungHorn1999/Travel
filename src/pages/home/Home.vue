97997fd8c9c53aea20581bf4107cc1d782f1cb00
YoungHorn1999 <1026344206@qq.com>
2020-07-31 10:57:46 +0800

    finish page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
     name: 'Home',
@@ -23,8 +24,14 @@ export default {
             swiperList: [],
             iconList: [],
             recommendList: [],
-            weekendList: []
+            weekendList: [],
+            lastCity: ''
         }
+    },
+    computed: {
+        ...mapState({
+            currentCity:'city'
+        })
     },
     components: {
         HomeHeader,
@@ -35,7 +42,7 @@ export default {
     },
     methods: {
         getHomeInfo () {
-            axios.get('/api/index.json')
+            axios.get('/api/index.json?city' + this.currentCity)
                 .then(this.getHomeInfoSucc)
         },
         getHomeInfoSucc (res) {
@@ -50,7 +57,14 @@ export default {
         }
     },
     mounted () {
+        this.lastCity = this.currentCity
         this.getHomeInfo()
+    },
+    activated () {
+        if(this.lastCity !== this.currentCity) {
+            this.lastCity = this.currentCity
+            this.getHomeInfo() 
+        }        
     }
 }
 </script>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,7 +9,7 @@
         </div>
         <router-link to="/city">
             <div class="header-right">
-                {{this.doubleCity}}
+                {{this.currentCity}}
                 <span class="iconfont arrow-icon">&#xe64a;</span>
             </div>
         </router-link>
@@ -17,16 +17,13 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 export default {
     name: 'HomeHeader',
     computed: {
         ...mapState({
             currentCity: 'city'
         }),
-        ...mapGetters({
-            doubleCity: 'doubleClick'
-        })
     },
     props: {
         city: String
