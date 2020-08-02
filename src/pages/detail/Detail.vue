fb27fd7dd0c81bee2ca20856feb50f4bc40b5848
YoungHorn1999 <1026344206@qq.com>
2020-08-02 11:46:27 +0800

    ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <detail-banner></detail-banner>
+        <detail-banner 
+            :sightName="sightName"
+            :bannerImg="bannerImg"
+            :gallaryImgs="gallaryImgs"></detail-banner>
         <detail-header></detail-header>
         <div class="content"></div>
     </div>
@@ -9,11 +12,40 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import axios from 'axios'
 export default {
     name: "Detail",
     components: {
         DetailBanner,
         DetailHeader
+    },
+    data () {
+        return {
+            sightName: '',
+            bannerImg: '',
+            gallaryImgs: '',
+            categoryList: []
+
+        }
+    },
+    methods: {
+        getDetailInfo () {
+            axios.get('api/detail.json', {
+                params: {
+                    id: this.$route.params.id
+                }
+            }).then(this.handleGetDataSucc)
+        },
+        handleGetDataSucc (res) {
+            res = res.data
+            if(res.ret && res.data){
+                const data = res.data
+                console.log(data)
+            }
+        }
+    },
+    mounted () {
+        this.getDetailInfo()
     }
 }
 </script>
